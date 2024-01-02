09f89560cb2b565d0c444d28b8d91ba1be48fce7
alicia-granados <aliciagranados35@gmail.com>
2024-01-02 17:23:11 -0600

    test: :sparkles: add test for ProcessSkins function

--- API/handlers/requests_test.go
+++ API/handlers/requests_test.go
@@ -63,11 +63,11 @@ func TestProcessChampions(t *testing.T) {
 			msg: nil,
 		},
 		{
-			name: "champion inserted correctly",
+			name: "skin inserted correctly",
 			args: args{
 				dbRepo: dbRepo,
 				infCampeon: Champion{
-					Id:    "Real-id",
+					Id:    "Real-skin-id",
 					Name:  "Real-name",
 					Title: "Real-title",
 					Skins: []Skins{},
@@ -135,7 +135,91 @@ func TestProcessTags(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := ProcessTags(tt.args.dbRepo, tt.args.tags, tt.args.championName)
-			fmt.Printf("error: %v\n", err)
+			if err != nil {
+				if err.Error() != tt.expectedError.Error() {
+					t.Errorf("%v: expected returned error to be %v", err, tt.expectedError)
+				}
+			}
+		})
+	}
+}
+
+func TestProcesSkins(t *testing.T) {
+	type args struct {
+		dbRepo       db.DatabaseRepo
+		skins        []Skins
+		championName string
+	}
+	dbRepo := &db.TestDBRepo{}
+	tests := []struct {
+		name          string
+		args          args
+		expectedError error
+	}{
+		{
+			name: "should panic when skin could not be found",
+			args: args{
+				dbRepo: dbRepo,
+				skins: []Skins{
+					{
+						Id_Num: "Wrong-fake-id",
+						Num:    0,
+						Name:   "Non-existant-fake-skin",
+					},
+				},
+				championName: "fake-name",
+			},
+			expectedError: fmt.Errorf("error getting the skin ID:there is an unexpected error"),
+		},
+		{
+			name: "found skin but no found champion",
+			args: args{
+				dbRepo: dbRepo,
+				skins: []Skins{
+					{
+						Id_Num: "Non-existant-fake-skin",
+						Num:    0,
+						Name:   "Non-existant-fake-name",
+					},
+				},
+				championName: "Wrong-fake-name",
+			},
+			expectedError: fmt.Errorf("error getting the champion ID:there is an unexpected error"),
+		},
+		{
+			name: "skins no exits ",
+			args: args{
+				dbRepo: dbRepo,
+				skins: []Skins{
+					{
+						Id_Num: "Non-existant-fake-skin",
+						Num:    0,
+						Name:   "skin-default",
+					},
+				},
+				championName: "Non-existant-fake-name",
+			},
+			expectedError: fmt.Errorf("error getting the skin ID:there is an unexpected error"),
+		},
+		{
+			name: "Champion has been found but the skin is repeated",
+			args: args{
+				dbRepo: dbRepo,
+				skins: []Skins{
+					{
+						Id_Num: "Non-existant-fake-skin",
+						Num:    0,
+						Name:   "Repeated-skin",
+					},
+				},
+				championName: "Real-name",
+			},
+			expectedError: fmt.Errorf("error inserting the skin: repeated skin"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := ProcesSkins(tt.args.dbRepo, tt.args.skins, tt.args.championName)
 			if err != nil {
 				if err.Error() != tt.expectedError.Error() {
 					t.Errorf("%v: expected returned error to be %v", err, tt.expectedError)
