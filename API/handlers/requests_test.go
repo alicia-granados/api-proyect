2c27e3e4f7adcedb2bfe15b94da12608b89dd088
alicia-granados <aliciagranados35@gmail.com>
2024-01-02 13:32:44 -0600

    test: :sparkles: add test process tags

--- API/handlers/requests_test.go
+++ API/handlers/requests_test.go
@@ -62,6 +62,21 @@ func TestProcessChampions(t *testing.T) {
 			},
 			msg: nil,
 		},
+		{
+			name: "champion inserted correctly",
+			args: args{
+				dbRepo: dbRepo,
+				infCampeon: Champion{
+					Id:    "Real-id",
+					Name:  "Real-name",
+					Title: "Real-title",
+					Skins: []Skins{},
+					Lore:  "Real-lore",
+					Tags:  []string{},
+				},
+			},
+			msg: nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -73,6 +88,59 @@ func TestProcessChampions(t *testing.T) {
 				}
 			}
 
+		})
+	}
+}
+
+func TestProcessTags(t *testing.T) {
+	type args struct {
+		dbRepo       db.DatabaseRepo
+		tags         []string
+		championName string
+	}
+	dbRepo := &db.TestDBRepo{}
+	tests := []struct {
+		name          string
+		args          args
+		expectedError error
+	}{
+		{
+			name: "should panic when champion could not be found",
+			args: args{
+				dbRepo:       dbRepo,
+				tags:         []string{"tag1", "tag1"},
+				championName: "Wrong-fake-name",
+			},
+			expectedError: fmt.Errorf("error getting the champion ID:there is an unexpected error"),
+		},
+		{
+			name: "champion has been found but the tags are empty",
+			args: args{
+				dbRepo:       dbRepo,
+				tags:         []string{"tag1"},
+				championName: "Real-name",
+			},
+			expectedError: fmt.Errorf("error inserting the tag:repeated tag"),
+		},
+		{
+			name: "tag inserted correctly",
+			args: args{
+				dbRepo:       dbRepo,
+				tags:         []string{"tagtest", "tagtest2"},
+				championName: "Real-name",
+			},
+			expectedError: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := ProcessTags(tt.args.dbRepo, tt.args.tags, tt.args.championName)
+			fmt.Printf("error: %v\n", err)
+			if err != nil {
+				if err.Error() != tt.expectedError.Error() {
+					t.Errorf("%v: expected returned error to be %v", err, tt.expectedError)
+				}
+			}
 		})
 	}
 }
