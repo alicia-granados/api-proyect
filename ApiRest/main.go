55f2010bbd68c53058474b8c1298622ff679c46a
alicia-granados <aliciagranados35@gmail.com>
2024-01-05 12:48:04 -0600

    feat: :sparkles: initialize server

--- ApiRest/main.go
+++ ApiRest/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Project start")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "¡Hello, world!")
+	})
+
+	// Especificar el puerto
+	PORT := ":8080"
+	fmt.Printf("Server running on port %s\n", PORT)
+
+	// Iniciar el servidor
+	log.Fatal(http.ListenAndServe(PORT, nil))
 }
