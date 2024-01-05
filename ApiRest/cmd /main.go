fddbf950c857f57a3e56878ec608730f384c8da7
alicia-granados <aliciagranados35@gmail.com>
2024-01-05 14:16:34 -0600

    feat: :sparkles:

--- ApiRest/cmd /main.go
+++ ApiRest/cmd /main.go
@@ -12,8 +12,12 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "¡Hello, world!")
 	})
-	dnsServer := config.DSNServer()
+
+	dnsServer, err := config.DSNServer()
 	fmt.Printf("listen server: %s", dnsServer)
+	if err != nil {
+		log.Fatal("Error configuring server:", err)
+	}
 
 	log.Fatal(http.ListenAndServe(dnsServer, nil))
 }
