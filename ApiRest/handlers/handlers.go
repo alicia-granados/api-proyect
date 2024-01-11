38a4179e4f8f3a878307af8ba1275d8417c77df8
alicia-granados <aliciagranados35@gmail.com>
2024-01-11 16:20:10 -0600

    feat: :sparkles: add handler to add the champions

--- ApiRest/handlers/handlers.go
+++ ApiRest/handlers/handlers.go
@@ -4,14 +4,16 @@ import (
 	"ApiRest/db"
 	"ApiRest/models"
 	"database/sql"
+	"encoding/json"
 	"net/http"
 )
 
 func AllChampions(databaseRepo *db.RealDBRepo, rw http.ResponseWriter, r *http.Request) {
 
 	champions, err := databaseRepo.ListChampions()
+
 	if err != nil {
-		// Aquí puedes agregar lógica adicional para determinar el tipo de error y enviar un mensaje de error específico
+		// Here you can add additional logic to determine the type of error and send a specific error message
 		if err == sql.ErrNoRows {
 			models.SendNotFound(rw)
 		} else {
@@ -20,4 +22,24 @@ func AllChampions(databaseRepo *db.RealDBRepo, rw http.ResponseWriter, r *http.R
 		return
 	}
 	models.SendData(rw, champions)
+}
+
+func CreateChampion(databaseRepo *db.RealDBRepo, rw http.ResponseWriter, r *http.Request) {
+
+	champion := models.Champion{}
+	decoder := json.NewDecoder(r.Body)
+
+	// Decode the JSON of the request body
+	if err := decoder.Decode(&champion); err != nil {
+		models.SendUnprocessableEntity(rw)
+		return
+	}
+	// Database insertion logic
+	if err := databaseRepo.InsertChampion(champion.Name, champion.Title, champion.Lore); err != nil {
+		models.SendUnprocessableEntity(rw)
+		return
+	}
+	// Reply with a message
+	models.SendData(rw, champion)
+
 }
