7160821edae778495545023500364bac59bc763b
alicia-granados <aliciagranados35@gmail.com>
2024-01-11 16:17:10 -0600

    feat: :sparkles: add query insert champion

--- ApiRest/db/champions.go
+++ ApiRest/db/champions.go
@@ -1,10 +1,10 @@
 package db
 
 import (
-	"ApiRest/entities"
+	"ApiRest/models"
 )
 
-type Champion []entities.Champion
+type Champion []models.Data
 
 func (r *RealDBRepo) ListChampions() (Champion, error) {
 	sql := "SELECT Champion.Id, Champion.Name, Champion.Title, Champion.Lore, Skins.Id, Skins.Id_Num,  Skins.Num,  Skins.Id_Champion,  Skins.Name,  Tags.Id, Tags.Id_Champion, Tags.Name  FROM Champion LEFT JOIN Skins ON Champion.Id = Skins.Id_Champion LEFT JOIN Tags ON Champion.Id = Tags.Id_Champion"
@@ -17,9 +17,9 @@ func (r *RealDBRepo) ListChampions() (Champion, error) {
 	defer rows.Close() // Make sure to close the rows at the end
 
 	for rows.Next() {
-		champion := entities.Champion{}
-		skin := entities.Skins{}
-		tag := entities.Tags{}
+		champion := models.Champion{}
+		skin := models.Skins{}
+		tag := models.Tags{}
 
 		err := rows.Scan(&champion.Id, &champion.Name, &champion.Title, &champion.Lore, &skin.Id, &skin.Id_Num, &skin.Num, &skin.Id_Champion, &skin.Name, &tag.Id, &tag.Id_Champion, &tag.Name)
 
@@ -27,12 +27,16 @@ func (r *RealDBRepo) ListChampions() (Champion, error) {
 			return nil, err
 		}
 
-		champion.Skins = []entities.Skins{}
-		champion.Tags = []entities.Tags{}
+		champion.Skins = []models.Skins{}
+		champion.Tags = []models.Tags{}
 
 		champion.Skins = append(champion.Skins, skin)
 		champion.Tags = append(champion.Tags, tag)
-		champions = append(champions, champion)
+
+		data := models.Data{
+			Champion: map[string]models.Champion{"Champion": champion},
+		}
+		champions = append(champions, data)
 
 	}
 
@@ -42,4 +46,10 @@ func (r *RealDBRepo) ListChampions() (Champion, error) {
 	}
 	return champions, nil
 
+}
+
+// InsertChampion inserts a champion into the database and returns its ID
+func (r *RealDBRepo) InsertChampion(name, title, lore string) error {
+	_, err := r.DB.Exec("INSERT INTO Champion (Name, Title, Lore) VALUES (?, ?, ?)", name, title, lore)
+	return err
 }
