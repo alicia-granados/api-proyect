76a8aca3fd779563918a552384dd0f5a2f35c6ec
alicia-granados <aliciagranados35@gmail.com>
2024-01-11 19:04:37 -0600

    feat: :sparkles: update name   GetChampionId

--- ApiRest/db/repository.go
+++ ApiRest/db/repository.go
@@ -7,7 +7,7 @@ type DatabaseRepo interface {
 	ListChampions() (Champion, error)
 	InsertChampion(name, title, lore string) error
 	GetTagID(tag string) (int, error)
-	GetChampionID(champion string) (int, error)
+	GetChampionId(champion string) (int, error)
 	InsertTag(championID int, tag string) error
 	GetSkinID(Id_Num string) (int64, error)
 	InsertSkins(Id_Num string, num, championID int, name string) error
