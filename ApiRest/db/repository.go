3ae08346343528b186cb74f0979d4c4f4752c76b
alicia-granados <aliciagranados35@gmail.com>
2024-01-12 14:36:53 -0600

    feat: :sparkles: add functions to the interface

--- ApiRest/db/repository.go
+++ ApiRest/db/repository.go
@@ -1,13 +1,18 @@
 package db
 
+import "ApiRest/models"
+
 type DatabaseRepo interface {
 	Connect()
 	Close()
 	Ping()
+	ExistsID(table string, id int) (int, error)
 	ListChampions() (Champion, error)
 	InsertChampion(name, title, lore string) error
 	GetTagID(tag string) (int, error)
 	GetChampionId(champion string) (int, error)
+	UpdateChampion(championID int, Champion models.Champion) error
+	DeleteChampion(championID int) error
 	InsertTag(championID int, tag string) error
 	GetSkinID(Id_Num string) (int64, error)
 	InsertSkins(Id_Num string, num, championID int, name string) error
