87a1d155a0d4f6f91aeaf76de7b480a7a1e11075
alicia-granados <aliciagranados35@gmail.com>
2024-01-22 17:57:10 -0600

    feat: :sparkles: add interface for champions, tags and skins

--- ApiRest/db/repository.go
+++ ApiRest/db/repository.go
@@ -7,13 +7,46 @@ type DatabaseRepo interface {
 	Close()
 	Ping()
 	ExistsID(table string, id int) (int, error)
-	ListChampions() (Champion, error)
+
+	ChampionRead
+	ChampionWriten
+	SkinRepoRead
+	SkinRepoWriten
+	TagRepoRead
+	TagRepoWriten
+}
+
+type ChampionRead interface {
+	GetChampions() (models.Champion, error)
+	GetChampionsDetailList() (models.Champion, error)
+	GetChampionByID(championId int) (models.Champion, error)
+	GetChampionDetailsByID(championId int) (models.Champion, error)
+}
+
+type ChampionWriten interface {
+	DeleteChampionByID(championID int) error
 	InsertChampion(name, title, lore string) error
-	GetTagID(tag string) (int, error)
-	GetChampionId(champion string) (int, error)
-	UpdateChampion(championID int, Champion models.Champion) error
-	DeleteChampion(championID int) error
+	UpdateChampionByID(championID int, champion models.Champion) error
+}
+
+type SkinRepoRead interface {
+	GetSkinList() (models.Skins, error)
+	GetSkinByID(skinID int) (models.Skins, error)
+}
+
+type SkinRepoWriten interface {
+	DeleteSkinByID(skinID int) error
+	UpdateSkinByID(skinID int, skin models.Skins) error
+	InsertSkin(idNum string, num, championID int, name string) error
+}
+
+type TagRepoRead interface {
+	GetTagsList() (models.Tags, error)
+	GetTagByID(tagID int) (models.Tags, error)
+}
+
+type TagRepoWriten interface {
+	DeleteTagByID(tagID int) error
 	InsertTag(championID int, tag string) error
-	GetSkinID(Id_Num string) (int64, error)
-	InsertSkins(Id_Num string, num, championID int, name string) error
+	UpdateTagByID(tagID int, tag models.Tags) error
 }
